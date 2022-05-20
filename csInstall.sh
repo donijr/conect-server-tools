fa062422a9227e7b2f8de7c04c28c4babe397945
donijr <donizetigmjr@gmail.com>
2022-05-19 23:17:07 -0300

    Comando mkdir tem que usar o sudo.

--- csInstall.sh
+++ csInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Instalador do programa CS iniciado"
-mkdir /opt/conect-server-tools/
+sudo mkdir /opt/conect-server-tools/
 sudo cp -r ./* /opt/conect-server-tools
 cd /opt/conect-server-tools
-sudo ln -s config/cs_completion  /etc/bash_completion.d/cs_completion
+sudo ln -s /opt/conect-server-tools/config/cs_completion  /etc/bash_completion.d/cs_completion
 cd $HOME/.local/bin
 ln -s /opt/conect-server-tools/cs cs
 echo "Instalador Finalizado"
